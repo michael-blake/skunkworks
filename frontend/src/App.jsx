792115534e4949713009aa4a1340babe4b5abdf8
Michael Blake <michaelblake@axiscare.com>
2026-02-18 16:33:38 -0600

    Improved App Description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,8 +30,15 @@ export default function App() {
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <p>Frontend: React + Vite</p>
-      <p>Backend: PHP</p>
+      <h2>Frontend</h2>
+      <p>React + Vite hosted on an AWS Amplify Instance</p>
+      <p>Deployment: Autopulls from GitHub, builds using amplify.yml script, deploys to app.michael-blake.com</p>
+      <h2>Backend</h2>
+      <p>PHP/API hosted on an AWS EC2 Instance</p>
+      <p>Deployment: Github Action triggers build in AWS ECR, then triggers an SSM command to deploy to the EC2 instance hosted at api.michael-blake.com</p>
+      <p>CORS is handled by Nginx and only allowed from the frontend domain</p>
+      <h2>TLDR</h2>
+      <p>Push to GitHub, Everything else is automated</p>
 
       <div className="card">
         <h2>Backend response</h2>
